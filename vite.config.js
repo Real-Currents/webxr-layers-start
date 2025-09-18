8933bcb6378e49c1a3c47c7e8faeefcce094d5ae
John <john@real-currents.com>
2025-09-18 15:26:21 -0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import shader from 'rollup-plugin-shader';
-import * as path from 'path';
-import fs from "fs";
-
-const local_certs = (fs.existsSync('./certs'));
 
 export default {
-    base: "",
-    build: {
-        rollupOptions: {
-            input: {
-                app: path.resolve(__dirname, './src/main.js'),
-            },
-        },
-    },
     define: {
        // "process.env.MapboxAccessToken": JSON.stringify(process.env.MapboxAccessToken)
     },
@@ -44,30 +31,8 @@ export default {
             }
         ]
     },
-    server: (!!local_certs) ? {
-        https: {
-            key: fs.readFileSync('certs/privkey.pem'), // make certs symbolic link to dir with certification files
-            cert: fs.readFileSync('certs/fullchain.pem'), // make certs symbolic link to dir with certification files
-        },
-        host: 'dev.real-currents.com', // Allow external access
-        port: 5173
-    } : {
-        https: true // Use in combo with basicSsl plugin; not needed for Vite 5+
-    },
-    plugins: (!!local_certs) ? [
-        shader({
-            // All match files will be parsed by default,
-            // but you can also specifically include/exclude files
-            include: [
-                '**/*.glsl',
-                '**/*.vs',
-                '**/*.fs'
-            ],
-            // specify whether to remove comments
-            removeComments: true,   // default: true
-        })
-    ] : [
-        /* If certs not available, use basicSsl plugin as workaround for HTTPS */
+    server: { https: true }, // Not needed for Vite 5+
+    plugins: [
         basicSsl({
             /** name of certification */
             name: 'test',
@@ -76,16 +41,5 @@ export default {
             // /** custom certification directory */
             // certDir: '/Users/.../.devServer/cert'
         }),
-        shader({
-            // All match files will be parsed by default,
-            // but you can also specifically include/exclude files
-            include: [
-                '**/*.glsl',
-                '**/*.vs',
-                '**/*.fs'
-            ],
-            // specify whether to remove comments
-            removeComments: true,   // default: true
-        })
     ]
 };
